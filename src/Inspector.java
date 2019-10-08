7f49ec52d45348ad28d9d11c17e1c1cd9d4ffd97
natejacko <njacko@shaw.ca>
2019-10-08 16:22:41 -0600

    Added printing of class,super-class,interface(s) names, and constructor name/param types/modifiers

--- src/Inspector.java
+++ src/Inspector.java
@@ -1,12 +1,55 @@
+import java.lang.reflect.Constructor;
+import java.lang.reflect.Modifier;
+import java.lang.reflect.Parameter;
 
-public class Inspector {
+public class Inspector 
+{
 
-    public void inspect(Object obj, boolean recursive) {
+    public void inspect(Object obj, boolean recursive) 
+    {
         Class c = obj.getClass();
+        System.out.println("Begin Inspection:\n");
         inspectClass(c, obj, recursive, 0);
     }
 
-    private void inspectClass(Class c, Object obj, boolean recursive, int depth) {
+    private void inspectClass(Class c, Object obj, boolean recursive, int depth) 
+    {
+    	// Class Name
+    	printWithTabs("Class Name: " + c.getName(), depth);
+    	
+    	// Super-class
+    	//TODO inspect super-class
+    	printWithTabs("Super-class Name: " + c.getSuperclass().getName(), depth);
+    	
+    	// Interfaces
+    	//TODO inspect interfaces recursively
+    	for (Class i : c.getInterfaces())
+    	{
+    		printWithTabs("Implements: " + i, depth);
+    	}
+    	
+    	// Constructors
+    	int i = 0;
+    	for (Constructor con : c.getDeclaredConstructors())
+    	{
+    		String conPrefix = "Constructor " + i++; 
+    		printWithTabs(conPrefix + " name: " + con.getName(), depth);
+    		for (Parameter p : con.getParameters())
+    		{
+    			printWithTabs(conPrefix + " parameter type: " +p.getType().getName(), depth);
+    		}
+    		int mod = con.getModifiers();
+    		printWithTabs(conPrefix + " modifiers: " + Modifier.toString(mod), depth);
+    		
+    	}
     }
 
+    private void printWithTabs(String output, int tabs)
+    {
+    	while(tabs-- > 0)
+    	{
+    		System.out.print("\t");
+    	}
+    	System.out.println(output);
+    }
 }
